97fd705c430d8a0793f5ddb809bd8541e25ae432
李钰湘 <7433469+li_yu_xiang_33@user.noreply.gitee.com>
2021-12-06 11:42:18 +0800

    music-server

---
+++ src/main/java/com/javaclimb/music/constant/Constants.java
@@ -0,0 +1,13 @@
+package com.javaclimb.music.constant;
+
+/**
+ * @author zhangyan
+ * @Title:
+ * @Description:
+ * @date 2021/11/111:51
+ */
+public class Constants {
+
+    public static final String RESOURCE_WIN_PATH ="D:\\music终版\\music-server";
+
+}
